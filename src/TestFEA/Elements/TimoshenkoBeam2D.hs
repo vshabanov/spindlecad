6a475872af1465cc78e5a67bd0c279be869f620c
Vladimir Shabanov <dev@vshabanov.com>
2008-05-12 18:00:19 +0000

    Чуть подправил TimoshenkoBeam2D. Теперь там матрица 6x6 с единицами на главной диагонали для X1 и X2

--- src/TestFEA/Elements/TimoshenkoBeam2D.hs
+++ src/TestFEA/Elements/TimoshenkoBeam2D.hs
@@ -19,15 +19,18 @@ import ElementMatrix
 import Material
 import CrossSection
 
--- | Currently Y & C components must be equal to zero
+-- | Currently Y & C components must be equal to zero. Force on X axis
+-- is ignored (1 in matrix diagonal).
 timoshenkoBeam2D :: Node.XYC -> Node.XYC -> Material -> CrossSection -> E
 timoshenkoBeam2D n1 n2 mat cs =
     linearElement
-    (matrix 4 $ map (ei/(l^3*(1+f))*)
-     [  12,        6*l,   -12,        6*l
-     , 6*l,  l^2*(4+f),  -6*l,  l^2*(2-f)
-     , -12,       -6*l,    12,       -6*l
-     , 6*l,  l^2*(2-f),  -6*l,  l^2*(4+f) ])
+    (matrix 6 $ map (ei/(l^3*(1+f))*)
+     [   1,    0,         0,  0,    0,         0
+     ,   0,   12,       6*l,  0,  -12,       6*l
+     ,   0,  6*l, l^2*(4+f),  0, -6*l, l^2*(2-f)
+     ,   0,    0,         0,  1,    0,         0
+     ,   0,  -12,      -6*l,  0,   12,      -6*l
+     ,   0,  6*l, l^2*(2-f),  0, -6*l, l^2*(4+f) ])
     (fi [i1,i2,i3,i4,i5,i6])
     where l  = abs $ x2 - x1
           ei = materialE mat * areaMomentOfInertia cs
